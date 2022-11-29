fa63d354dd69a1ef572fe36d5c6b07970ac8b404
Mohib ul haq <muhibhaq123@gmail.com>
2022-11-29 16:39:39 +0500

    Added Review Model basic review styles and validation

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 const path = require('path');
 const engine = require('ejs-mate');
 const mongoose = require('mongoose');
-const { campgroundSchema } = require('./schemas')
+const { reviewSchema, campgroundSchema } = require('./schemas')
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
 const Campground = require('./models/campgrounds');
 const methodOverride = require('method-override');
+const Review = require('./models/reviews')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -24,9 +25,19 @@ app.set('views', path.join(__dirname, '/views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-const validation = (req, res, next) => {
+const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
-    console.log(error.details)
+
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
 
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
@@ -55,18 +66,25 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-app.post('/campgrounds', validation, catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-app.put('/campgrounds/:id', validation, catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     if (!req.body.campground) throw new ExpressError("Invalid Data", 400)
     const { id } = req.params;
     const campground = await Campground.findById(id);
     await Campground.findByIdAndUpdate(id, req.body.campground);
     res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`)
 }))
 
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
@@ -75,9 +93,18 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds')
 }))
 
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params
-    const campground = await Campground.findById(id);
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate('reviews');
     res.render('campgrounds/show', { campground })
 }))
 
@@ -86,7 +113,8 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { message, statusCode } = err;
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
 })
 
--- models/campgrounds.js
+++ models/campgrounds.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const reviews = require('./reviews')
 
 const Schema = mongoose.Schema;
 
@@ -7,7 +8,21 @@ const campgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
-    image: String
+    image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+})
+
+campgroundSchema.post('findOneAndDelete', async (camp) => {
+    if (camp) {
+        await reviews.deleteMany({
+            _id: { $in: camp.reviews }
+        })
+    }
 })
 
 module.exports = mongoose.model('Campground', campgroundSchema)
