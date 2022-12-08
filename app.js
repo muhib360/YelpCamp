2cce13e5529fe3fabe678a692d9c7a7e966e0acc
Mohib ul haq <muhibhaq123@gmail.com>
2022-12-08 13:26:31 +0500

    Routes restructures and Added FLASH messages

--- app.js
+++ app.js
@@ -1,115 +1,63 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const engine = require('ejs-mate');
 const mongoose = require('mongoose');
-const { reviewSchema, campgroundSchema } = require('./schemas')
-const catchAsync = require('./utils/catchAsync');
-const ExpressError = require('./utils/ExpressError')
-const Campground = require('./models/campgrounds');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Review = require('./models/reviews')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {});
 
 const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
-db.on('error', console.error.bind(console, 'Something went Wrong :('))
-db.once('connected', () => console.log('Successfuly Connected'))
+const app = express();
 
-app.engine('ejs', engine);
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+app.use(session(sessionConfig))
+app.use(flash());
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
 
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-}))
-
-app.get('/campgrounds/new', catchAsync(async (req, res) => {
-    res.render('campgrounds/new');
-}))
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const { id } = req.params
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/edit', { campground })
-}))
-
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    if (!req.body.campground) throw new ExpressError("Invalid Data", 400)
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    await Campground.findByIdAndUpdate(id, req.body.campground);
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`)
-}))
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds')
-}))
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews');
-    res.render('campgrounds/show', { campground })
-}))
 
 app.all('*', (req, res, next) => {
-    next(new ExpressError("Page not Found", 404))
+    next(new ExpressError('Page Not Found', 404))
 })
 
 app.use((err, req, res, next) => {
@@ -119,5 +67,7 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(3000, () => {
-    console.log('listening on port')
+    console.log('Serving on port 3000')
 })
+
+
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,21 +2,19 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const Campground = require('../models/campgrounds');
-const Review = require('../models/reviews');
-
-const { reviewSchema } = require('../schemas.js');
-
-
+const Review = require('../models/campgrounds');
+const { reviewSchema } = require('../schemas');
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
 const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
+
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
     } else {
-        next();
+        next()
     }
 }
 
